e2682e3b35423e74134de6289bcc73ed7374788b
GIGABYTE <rimu.appdeveloper@gmail.com>
2024-09-27 23:37:32 +0600

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 
 
-import 'package:car_class_live_test/car_class.dart';
+
+
+import 'car_class.dart';
 
 void main(){
   Car myCar = Car(brand: " Toyota",model: "Corolla",year:2015 );
 
-  // Displaying car information
+  // Show car information
 
   print('Brand: ${myCar.brand}');
   print('Model: ${myCar.model}');
